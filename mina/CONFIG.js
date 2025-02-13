a6f9ec72f705c1d825d916bbfd0adab4d453fab2
Mohammed Amer <53377619+OldMohamm@users.noreply.github.com>
2025-02-14 00:13:23 +0300

    Add files via upload

---
+++ mina/CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hello, I have something to say. </br> Will you be my lover mina?",
+  descriptionHeader:
+    "Please answer sincerely. If you stay silent and leave, it means you have AGREED.",
+  buttonYes: "I agree",
+  buttonNo: "I don't agree",
+  titleModar: "Thank you.",
+  descriptionModar:
+    "Thank you for giving me a chance to be by your side, to care for and protect you. </br> I have a sincere heart for you mina. You are my whole world. </br> الباجر رح ادزلج شي على طلبات او توترز عود اشوف شنو اقرر و ما يهمني اهلج 😂 </br> Love you <3",
+};
